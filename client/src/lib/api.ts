e7ccc177f2f80d9e02f14f147aace11fba425482
karTik-kuMar04 <mysoulisinfinity1@gmail.com>
2026-04-08 13:48:02 +0530

    fix: name backend url variable

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: process.env.BACKEND_BASE_URL,
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
