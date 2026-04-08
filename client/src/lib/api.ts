5a8f8b38208286163cb8480b79384e2cb70feb28
karTik-kuMar04 <mysoulisinfinity1@gmail.com>
2026-04-08 13:37:18 +0530

    Fix: added backend url from env file

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: process.env.BACKEND_BASE_URL,
     headers: {
         "Content-Type": "application/json",
     },
